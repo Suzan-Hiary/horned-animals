976ff5455005aa733e2d94503ac371c021c98c1e
Suzan-Hiary <suze-hiary@hotmail.com>
2021-08-02 19:16:23 +0300

    added lab-01

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+
+class Main extends Component {
+    render() {
+        return (
+            <div class="column">
+                <div class="row">
+                <img alt ='img1' src='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg' title='UniWhal'></img>
+                    <h1>UniWhal</h1> 
+                    <p>A unicorn and a narwhal nuzzling their horns,</p> 
+                    <p>narwhal</p> 
+                    <p>horns: 1</p>
+                    </div>
+                    <div class="row">
+                    <img src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"></img>
+                    <h1>Rhino Family</h1> 
+                    <p>Mother (or father) rhino with two babies</p> 
+                    <p>rhino</p> 
+                    <p>horns: 2</p> 
+                    </div>
+                    <div class="row">
+                        <img src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" alt="img3" title="img3"></img>
+                        <h1>Unicorn Head</h1> 
+                    <p>Someone wearing a creepy unicorn head mask</p> 
+                    <p>unicorn</p> 
+                    <p>horns: 1</p>
+                    </div>
+        </div>
+                )
+    }
+}
+
+                export default Main;
