4577dbd908998f983b857dd4e016e418d411e30a
Suzan-Hiary <suze-hiary@hotmail.com>
2021-08-03 22:17:17 +0300

    solved lab03

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
-class SelectedBeast extends React.Component  {
+
+
+class SelectedBeast extends React.Component {
+
     render() {
-        console.log(this.props.image_url);
-        return (
-            <>
-                <Modal show={this.props.show} onHide={this.props.onHide}>
-                    <Modal.Header>
-                        <Modal.Title>{this.props.title} </Modal.Title>
-                    </Modal.Header>
-                    <img  src={this.props.image_url} />
-                    <Modal.Body> {this.props.description}</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="danger" onClick={this.props.onHide}>
-                            Close
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </>
+        return(
+        <Modal show={this.props.show} onHide={this.props.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{this.props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>   <img
+            src={this.props.image_url}
+            alt={this.props.title}
+            
+          /></Modal.Body>
+        <Modal.Footer>
+        {this.props.description}
+        </Modal.Footer>
+      </Modal>
         )
     }
+
 }
 
 export default SelectedBeast;
